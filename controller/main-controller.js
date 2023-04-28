c8927173b6c551f31ad56840ca49f884326b99b8
Anas okasha <aokasha@smartchargetech.com>
2023-04-28 19:26:33 +0300

    handle scraping server side

--- controller/main-controller.js
+++ controller/main-controller.js
@@ -1,7 +1,34 @@
-const test =() =>{
-    console.log('welcome in web scraping using node js')
-}
+
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+    const getAllComputersData = async () =>{
+        try {
+            // here I am using Open sooq to scrape Computers Data
+            const baseUrl = 'https://jo.opensooq.com/en/computers-and-laptops/laptops';
+            const data = [];
+
+            // only bring 10 pages 
+            for (let i = 1; i < 20; i++) {
+              const url = `${baseUrl}?page=${i}`;
+              const response = await axios.get(url);
+              const $ = cheerio.load(response.data);
+              const items = $('.mb-32.relative');
+        
+              items.each((index, element) => {
+                  const value = $(element).find('.flex.flexSpaceBetween > h2').text();
+                  const title = $(element).find('.postPrice.ms-auto.bold.alignSelfCenter.font-19').text();
+                  const imageLink = $(element).find('.sc-cbd35298-1.cSfYeM.postImgCont.relative.radius-8.overflowHidden.me-16 > img').attr('src');
+                data.push({index, title, value, imageLink});
+              });
+            }
+        
+            return data
+        } catch(error) {
+            console.log(error)
+        }
+    }
 
 module.exports = {
-    test
+    getAllComputersData
 }
--- router/main-route.js
+++ router/main-route.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const mainRouter = express.Router();
-const {test} = require('../controller/main-controller');
+const { getAllComputersData } = require('../controller/main-controller');
 
-mainRouter.get('/', test);
+
+mainRouter.get('/',async(req,res)=>{
+    const data = await getAllComputersData();
+    const rows = data.map(({ index,title, value,imageLink }) => {
+    return `<tr><td>${index}</td><td>${title}</td><td>${value}</td><td><img src="${imageLink}"></td></tr>`;
+        }).join('');
+    const table = `<table>${rows}</table>`;
+    res.send(table);
+})
 
 module.exports = mainRouter;
